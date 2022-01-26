45f77def058c65df04b4e164bdb786d22bab8bee
BarkingDevelopment <max@the-barkers.com>
2022-01-26 16:13:24 +0000

    Added "role" column in user table to allow for identification of different types of users. Initially this is for differentiation between normal users and admins but can later be used to differentiate between types of user.

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,6 +40,7 @@ class User extends Authenticatable
         'id',
         'password',
         'remember_token',
+        'is_admin',
     ];
 
     /**
---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param User $user
+     * @return bool
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param User $user
+     * @param User $model
+     * @return bool
+     */
+    public function view(User $user, User $model): bool
+    {
+        return $user->id === $model->id;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param User $user
+     * @return bool|false
+     */
+    public function create(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param User $user
+     * @param User $model
+     * @return bool
+     */
+    public function update(User $user, User $model): bool
+    {
+        return $user->id === $model->id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param User $user
+     * @param User $model
+     * @return bool
+     */
+    public function delete(User $user, User $model): bool
+    {
+        return $user->id === $model->id;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param User $user
+     * @param User $model
+     * @return bool|false
+     */
+    public function restore(User $user, User $model): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     * I'd like to make this return false but it goes against GDPR.
+     *
+     * @param User $user
+     * @param User $model
+     * @return bool
+     */
+    public function forceDelete(User $user, User $model): bool
+    {
+        return $user->id === $model->id;
+    }
+}
