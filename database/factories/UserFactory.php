05dd6e960abe7f7698f1075e3e4be5c2ee565027
BarkingDevelopment <max@the-barkers.com>
2022-01-26 16:13:24 +0000

    Added "role" column in user table to allow for identification of different types of users. Initially this is for differentiation between normal users and admins but can later be used to differentiate between types of user.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\UserRole;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
@@ -28,6 +29,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => $this->faker->password(),
             'remember_token' => Str::random(10),
+            'role' => UserRole::Member(),
         ];
     }
 
